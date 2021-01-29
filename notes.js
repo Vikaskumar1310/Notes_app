3aa9bef10bf70e8a7f742577a16f0a551bcd616c
vikas kumar <vkk1310@gmail.com>
2021-01-29 10:35:20 +0530

    comments added

--- notes.js
+++ notes.js
@@ -1,19 +1,21 @@
+//importing required modules
 const fs = require('fs');
-const chalk = require('chalk');
+const chalk = require('chalk'); //for interective output on console
 
 //addNote to notes file
 const addNote = (title, body) => {
-    const notes = loadNotes();
-    const duplicateNote = notes.find((note) => note.title === title);
+    const notes = loadNotes();//loading notes
+    const duplicateNote = notes.find((note) => note.title === title);//finding and adding the title which is already present in notes
 
+    //if title is not present we will add into app
     if (!duplicateNote) {
-        notes.push({
+        notes.push({ // pushing title or body into notes  i.e array
             title: title,
             body: body
         })
-        saveNotes(notes);
-        console.log(chalk.green.inverse("New note added"));
-    } else {
+        saveNotes(notes);//saving notes
+        console.log(chalk.green.inverse("New note added"));//for interective output
+    } else { //if notes already present with title will show error
         console.log(chalk.red.inverse("Note title is already taken!"));
     }
 }
@@ -56,23 +58,25 @@ const readNote = (title) => {
 
 //save note to file
 const saveNotes = (notes) => {
-    const dataJson = JSON.stringify(notes);
-    fs.writeFileSync('notes.json', dataJson);
+    const dataJson = JSON.stringify(notes);//converitng the title and body into json format
+    fs.writeFileSync('notes.json', dataJson);//data adding into file 
 }
 
 //load all present notes in the file
 const loadNotes = () => {
+    //if file is empty or no file available give error i.e emppty array 
+    //if file is available ,will load all notes present in that file
     try {
-        const dataBuffer = fs.readFileSync('notes.json');
-        const dataJson = dataBuffer.toString();
-        return JSON.parse(dataJson);
+        const dataBuffer = fs.readFileSync('notes.json');//reading the file 
+        const dataJson = dataBuffer.toString();//converting data into string 
+        return JSON.parse(dataJson);//now here we are parsing the data into json format.
     } catch (e) {
         return [];
     }
 
 }
 
-
+//exporting required modules
 module.exports = {
     addNote: addNote,
     removeNote: removeNote,
